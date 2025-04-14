5894934da310cacebc9a259feceb56bae3020c84
Alan Sartorio <alan42ga@hotmail.com>
2025-04-14 09:09:03 -0600

    code format

--- configuration.nix
+++ configuration.nix
@@ -18,7 +18,8 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  boot.initrd.luks.devices."luks-97440c40-cde6-47b0-97ba-bb3ac82b6e3c".device = "/dev/disk/by-uuid/97440c40-cde6-47b0-97ba-bb3ac82b6e3c";
+  boot.initrd.luks.devices."luks-97440c40-cde6-47b0-97ba-bb3ac82b6e3c".device =
+    "/dev/disk/by-uuid/97440c40-cde6-47b0-97ba-bb3ac82b6e3c";
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
