5fca6398fa163901849181f606cccd349ab24657
Alan Sartorio <alan42ga@hotmail.com>
2025-09-25 06:59:28 -0300

    add systemd-resolved for dns

--- services.nix
+++ services.nix
@@ -127,4 +127,15 @@ in
       ServerAlias *
     '';
   };
+
+  services.resolved = {
+    enable = true;
+    #dnssec = "true";
+    domains = [ "~." ];
+    fallbackDns = [
+      "1.1.1.1"
+      "1.0.0.1"
+    ];
+    #dnsovertls = "true";
+  };
 }
